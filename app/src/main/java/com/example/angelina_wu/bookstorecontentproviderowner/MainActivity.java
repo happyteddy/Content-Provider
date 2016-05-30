3de96832d19cd13cbb643ef0e70a58fcdc3af9f8
Angelina_Wu <Angelina_Wu@asus.com>
2016-05-30 10:23:07 +0800

    Cursorloader

--- app/src/main/java/com/example/angelina_wu/bookstorecontentproviderowner/MainActivity.java
+++ app/src/main/java/com/example/angelina_wu/bookstorecontentproviderowner/MainActivity.java
@@ -5,26 +5,25 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.os.Build;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import static android.provider.BaseColumns._ID;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
-
+    private SimpleCursorAdapter mDataAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("Activity","- onCreate");
         setContentView(R.layout.activity_main);
         //add();
+        getSupportLoaderManager().initLoader(0, null, this);
         display();
     }
     public void add() {  // test
@@ -44,13 +43,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
     public void display() {
-
-        //try (Cursor c = getContentResolver().query(BookStoreContentProvider.CONTENT_URI, null, null, null, null )) {
-            Cursor c = getContentResolver().query(BookStoreContentProvider.CONTENT_URI, null, null, null, null );
-            startManagingCursor(c);
-            if (c != null) {
-                c.moveToFirst();
-            }
 
             String[] columns = new String[] {
                     _ID,
@@ -73,26 +65,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                     R.id.buyer_id
             };
 
-            SimpleCursorAdapter dataAdapter = new SimpleCursorAdapter(this, R.layout.book_info,c,columns,to,0);
+            mDataAdapter = new SimpleCursorAdapter(this, R.layout.book_info,null,columns,to,0);
             ListView list = (ListView) findViewById(R.id.listView);
             if( null != list ) {
-                list.setAdapter(dataAdapter);
+                list.setAdapter(mDataAdapter);
+                getSupportLoaderManager().restartLoader(0, null, this);
             }
-        //}
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        CursorLoader cl = new CursorLoader(this, BookStoreContentProvider.CONTENT_URI,
+                null, null, null, null);
+        return cl;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+        mDataAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        mDataAdapter.swapCursor(null);
     }
 }
